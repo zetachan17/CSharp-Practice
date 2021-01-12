da27b48fd700c80964adfb1b8f807fd9d4f43748
Tyler Zhu <67493075+zetachan17@users.noreply.github.com>
2021-01-12 15:05:35 -0500

    Hello World
    Fuck this world actually

---
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fuck this World");
+            Console.Read();
+        }
+    }
+}
